db15464cd45fd3e3751c208556a8db2b6fc041ca
Travis Vachon <travis@copious.com>
2013-11-24 15:35:18 -0500

    0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject parseapp/lein-template "0.3.0"
+(defproject parseapp/lein-template "0.4.0"
   :description "A leiningnen template for creating Parse apps"
   :url "http://github.com/travis/parseapp"
   :license {:name "Eclipse Public License"
